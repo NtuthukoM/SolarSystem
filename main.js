f25991f7cf5c4b353561ce2735e27abe18b58f86
NtuthukoM <ntuthukorocks@gmail.com>
2024-08-19 20:53:40 +0200

    rotating sun

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add(cube);
 
-camera.position.z = 5;
+const geometry = new THREE.SphereGeometry( 1, 32, 16 ); 
+const material = new THREE.MeshBasicMaterial( { color: 'yellow' } );
+const sun = new THREE.Mesh( geometry, material );
+scene.add(sun);
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
+const controls = new OrbitControls( camera, renderer.domElement );
+camera.position.z = 5;
+controls.update();
+const loader = new GLTFLoader();
+
 document.body.appendChild( renderer.domElement );
 function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    sun.rotation.x += 0.01;
+    sun.rotation.y += 0.01;
+    controls.update();
 	renderer.render( scene, camera );
 }
 renderer.setAnimationLoop( animate );
